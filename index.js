3b1d664759d32efaac866eefddacf78b99c0a163
Anurag <kumarayush1014@gmail.com>
2024-01-25 10:55:27 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+
+app.get('/.well-known/assetlinks.json',async (req,res) => {
+    res.json([
+        {
+            "relation": [
+                "delegate_permission/common.handle_all_urls"
+            ],
+            "target": {
+                "namespace": "android_app",
+                "package_name": process.env.PACKAGE,
+                "sha256_cert_fingerprints": [
+                    process.env.SHA
+                ]
+            }
+        }
+    ]);
+})
+
+app.listen(3000,() => {
+    console.log('Server running on port 3000');
+})
